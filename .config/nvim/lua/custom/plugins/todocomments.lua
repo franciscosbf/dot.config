dc338ccebcdcd8a5b18c52ebdd5b794c640d2824
francisco <f.s.f.b2001@gmail.com>
2023-05-13 15:30:10 +0100

    add todo plugin keymap to list todo comments with the trouble plugin

--- .config/nvim/lua/custom/plugins/todocomments.lua
+++ .config/nvim/lua/custom/plugins/todocomments.lua
@@ -1,10 +1,23 @@
 return {
   'folke/todo-comments.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {
-    -- configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- useful for for trouble
+    'folke/trouble.nvim'
   },
+  config = function ()
+    require('todo-comments').setup({
+      -- configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    })
+
+    local map = vim.api.nvim_set_keymap
+
+    map(
+      'n', '<leader>tt', '<Cmd>TodoTrouble<CR>',
+      { noremap = true, silent = true, desc = 'Todo Trouble' }
+    )
+  end
 }
 
