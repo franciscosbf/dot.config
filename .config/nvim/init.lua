f081f950b4cbe737ef3613da0bf3c44b6a75643d
francisco <f.s.f.b2001@gmail.com>
2023-05-15 12:55:34 +0100

    change nvim tab width

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -270,9 +270,9 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- Set tab width
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 -- Add comment on newline
