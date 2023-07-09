978f2b183d2ac91104d5180a3086f867aa0ed362
francisco <f.s.f.b2001@gmail.com>
2023-07-09 03:10:20 +0100

    add gruvbox-material and catppuccin. set theme to gruvbox-material

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,61 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add gruvbox material
+  {
+    "sainnhe/gruvbox-material",
+    lazy = true,
+    config = function()
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_better_performance = 1
+    end,
+  },
+
+  -- add catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
