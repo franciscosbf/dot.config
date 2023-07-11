3de0c8a17bed75827e0190fea6f9f5ed6767cd4f
francisco <f.s.f.b2001@gmail.com>
2023-07-11 14:52:18 +0100

    add toggleterm

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<A-\>]],
+      direction = "float",
+      float_opts = {
+        border = "single",
+        winblend = 0,
+      },
+    },
+  },
+}
