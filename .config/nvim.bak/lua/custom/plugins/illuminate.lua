03376c866c34340b8e2fd67cdd7af5acd39a7f11
francisco <f.s.f.b2001@gmail.com>
2023-07-12 22:07:03 +0100

    remove ugly *** nvim bk

--- .config/nvim.bak/lua/custom/plugins/illuminate.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'RRethy/vim-illuminate',
-  config = function ()
-    require('illuminate').configure({
-      -- config goes here
-      under_cursor = false,
-    })
-  end
-}
--- .config/nvim.bak/py3env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Creates a python virtual environment.
-
-python3 -m env py3env && \
-	source py3env/bin/activate && \
-	python3 -m pip install -U pip pynvim
-
