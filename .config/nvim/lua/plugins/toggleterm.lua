df143fdd8a055355ba294d8c99a57eba93b794d1
francisco <f.s.f.b2001@gmail.com>
2023-08-17 21:09:59 +0100

    change toggleterm open mapping to A-q

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {
-      open_mapping = [[<A-\>]],
+      open_mapping = [[<A-q>]],
       direction = "float",
       float_opts = {
         border = "single",
