d372da36f02175c0106d0cb78f2f3a21afe37caa
francisco <f.s.f.b2001@gmail.com>
2023-08-26 12:05:33 +0100

    nvim: disable ruby and perl providers

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,3 +7,9 @@ vim.opt.colorcolumn = "80"
 
 -- add python dedicated environment with pynvim installed.
 vim.g.python3_host_prog = "~/.config/nvim-env/bin/python3"
+
+-- disable ruby provider
+vim.g.loaded_ruby_provider = 0
+
+-- disable perl provider
+vim.g.loaded_perl_provider = 0
