95e4d4ef1d2ca9262b7e03b10da09d23edf32d29
francisco <f.s.f.b2001@gmail.com>
2023-08-31 20:42:20 +0100

    set semanticTokens and usePlaceholders + Workaround for the lack of a DAP strategy in neotest-go

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -64,6 +64,10 @@ return {
     opts = {
       servers = {
         gopls = {
+          keys = {
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+          },
           settings = {
             gopls = {
               gofumpt = true,
@@ -93,10 +97,11 @@ return {
                 unusedwrite = true,
                 useany = true,
               },
-              usePlaceholders = false,
+              usePlaceholders = true,
               completeUnimported = true,
               staticcheck = true,
               directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
             },
           },
         },
