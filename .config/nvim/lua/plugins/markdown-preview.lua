ce0176a1f433d0a29ffc905c953e0f63146e275a
francisco <f.s.f.b2001@gmail.com>
2023-09-05 13:29:32 +0100

    nvim: add markdown preview toggle key to its config

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -6,6 +6,9 @@ return {
     build = function()
       vim.fn["mkdp#util#install"]()
     end,
+    keys = {
+      { "<leader>p", "<Cmd>MarkdownPreviewToggle<CR>", desc = "Opens markdown preview" },
+    },
     config = function()
       vim.g.mkdp_theme = "dark"
     end,
