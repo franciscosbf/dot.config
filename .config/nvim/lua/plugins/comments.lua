7429617255f8b11e63bb3d85dc2083b989f408fa
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-09-28 15:10:03 +0100

    nvim: replace mini.comment by Comment.nvim

---
+++ .config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,11 @@
+return {
+  -- Replace Comment by mini.comment
+  { "echasnovski/mini.comment", enabled = false },
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = {
+      -- add any options here
+    },
+  },
+}
