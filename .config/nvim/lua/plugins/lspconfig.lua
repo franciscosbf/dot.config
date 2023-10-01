10ac6da6d91afe49fa6e4f92506e24d352a15e50
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-10-01 21:47:25 +0100

    nvim: change RustHoverActions command key

---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        rust_analyzer = {
+          keys = {
+            { "<leader>ar", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+            { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+            { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+          },
+        },
+      },
+    },
+  },
+}
