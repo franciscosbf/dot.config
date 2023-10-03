1822e4734180f7cd8b93baf2e5ef108774f7aca0
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-10-03 10:51:56 +0100

    nvim: disable autoformat for go files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,9 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable autoformat for rust files
+-- Disable autoformat for rust and go files.
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "rust" },
+  pattern = { "rust", "go" },
   callback = function()
     vim.b.autoformat = false
   end,
