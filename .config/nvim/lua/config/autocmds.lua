4898727c716a0c8ba2c088c9ebad8a08088889b8
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-11-01 16:51:22 +0000

    nvim: disable autoformat for sh files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,9 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable autoformat for rust, go, c and cuda files.
+-- Disable autoformat for rust, go, c, cuda and sh files.
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "rust", "go", "c", "cuda" },
+  pattern = { "rust", "go", "c", "cuda", "sh" },
   callback = function()
     vim.b.autoformat = false
   end,
