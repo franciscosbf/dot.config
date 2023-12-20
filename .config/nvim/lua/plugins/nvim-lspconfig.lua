bf7031bb9775ec381fbdcc1f8d1b7b27c9668709
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2023-12-20 19:14:49 +0000

    nvim: refactor nvim rust lspconfig and disable inlay hints and lens

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,9 +5,23 @@ return {
       servers = {
         rust_analyzer = {
           keys = {
+            -- change default key binds.
             { "<leader>ar", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
             { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
             { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+          },
+          settings = {
+            ["rust-analyzer"] = {
+              -- get rid of inlay hints and lens.
+              inlayHints = {
+                chainingHints = { enable = false },
+                closingBraceHints = { enable = false },
+                parameterHints = { enable = false },
+                renderColons = { enable = false },
+                typeHints = { enable = false },
+              },
+              lens = { enable = false },
+            },
           },
         },
         erlangls = {},
