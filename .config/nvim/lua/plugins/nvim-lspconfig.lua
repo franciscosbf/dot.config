4c5b666d8ea19bfdd3f0f6940c5bf6d60320584b
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-01-15 11:15:26 +0000

    nvim: remove key bindings and inlay hints from rust_analyzer

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,22 +4,9 @@ return {
     opts = {
       servers = {
         rust_analyzer = {
-          keys = {
-            -- change default key binds.
-            { "<leader>ar", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
-            { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
-            { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
-          },
           settings = {
             ["rust-analyzer"] = {
-              -- get rid of inlay hints and lens.
-              inlayHints = {
-                chainingHints = { enable = false },
-                closingBraceHints = { enable = false },
-                parameterHints = { enable = false },
-                renderColons = { enable = false },
-                typeHints = { enable = false },
-              },
+              -- get rid of lens.
               lens = { enable = false },
             },
           },
