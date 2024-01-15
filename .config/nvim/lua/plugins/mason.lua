82239460aa6b9d7ceceb3c6239072154de23ecb9
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-01-15 10:35:31 +0000

    nvim: remove mason unecessary opts

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,10 +1,5 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, "shellcheck")
-      table.insert(opts.ensure_installed, "shfmt")
-    end,
   },
 }
