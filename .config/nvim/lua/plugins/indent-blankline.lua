7d9ded8b7ef8e4a44445f7d3942827251faf94dd
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-05-18 01:34:17 +0100

    nvim: fix indent-blankline plugin version in 3.5.4
    until neovim 0.10 is uploaded to arch linux stable repo

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,8 @@
+return {
+  -- fix version since the new one only suports neovim 0.10
+  -- https://github.com/lukas-reineke/indent-blankline.nvim/issues/869#issuecomment-2117099929
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    version = "3.5.4",
+  },
+}
