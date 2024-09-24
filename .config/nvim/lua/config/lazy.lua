f65bbc4db375da6b2d8e13ae3261f8fa7ce5acaf
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-09-24 13:24:12 +0100

    nvim: add java extra plugin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.tex" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     -- import/override with your plugins
     { import = "plugins" },
   },
