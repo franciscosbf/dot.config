d7d847522eff6b2b8d5e4156bc1106c6319368d0
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-09-26 18:28:27 +0100

    nvim: disable autoformat for markdown files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -4,7 +4,7 @@
 
 -- Disable autoformat for all kind of files.
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "cpp", "cmake", "cuda", "html" },
+  pattern = { "markdown", "c", "cpp", "cmake", "cuda", "html" },
   callback = function()
     vim.b.autoformat = false
   end,
