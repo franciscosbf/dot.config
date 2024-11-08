af503bfd548d351c88c36082f0882d7c08bb10e8
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-11-08 00:24:17 +0000

    nvim: disable semanticTokens and usePlaceholders in gopls lspconfig

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,22 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              semanticTokens = false,
+              usePlaceholders = false,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
       inlay_hints = { enabled = false },
     },
   },
