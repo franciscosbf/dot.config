f43bf4958d7d1ee0b3dd23b398f0dc0cc76f7788
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2024-11-12 18:56:53 +0000

    nvim: move bufferline key binds to keymaps.lua and map notification history

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "Toggle pin" })
+map("n", "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", { desc = "Delete non-pinned buffers" })
+map("n", "<A-,>", "<cmd>BufferLineCyclePrev<cr>")
+map("n", "<A-.>", "<cmd>BufferLineCycleNext<cr>")
+map("n", "<A-<>", "<cmd>BufferLineMovePrev<cr>")
+map("n", "<A->>", "<cmd>BufferLineMoveNext<cr>")
+map("n", "<A-1>", "<cmd>BufferLineGoToBuffer  1<cr>")
+map("n", "<A-2>", "<cmd>BufferLineGoToBuffer  2<cr>")
+map("n", "<A-3>", "<cmd>BufferLineGoToBuffer  3<cr>")
+map("n", "<A-4>", "<cmd>BufferLineGoToBuffer  4<cr>")
+map("n", "<A-5>", "<cmd>BufferLineGoToBuffer  5<cr>")
+map("n", "<A-6>", "<cmd>BufferLineGoToBuffer  6<cr>")
+map("n", "<A-7>", "<cmd>BufferLineGoToBuffer  7<cr>")
+map("n", "<A-8>", "<cmd>BufferLineGoToBuffer  8<cr>")
+map("n", "<A-9>", "<cmd>BufferLineGoToBuffer  9<cr>")
+map("n", "<A-0>", "<cmd>BufferLineGoToBuffer -1<cr>")
+map("n", "<A-c>", "<cmd>BufferLinePickClose<cr>")
+
+map("n", "<leader>n", function()
+  Snacks.notifier.show_history()
+end, { desc = "Show notification history" })
--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,29 +2,6 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-      -- Move to previous/next
-      { "<A-,>", "<Cmd>BufferLineCyclePrev<CR>" },
-      { "<A-.>", "<Cmd>BufferLineCycleNext<CR>" },
-      -- Re-order to previous/next
-      { "<A-<>", "<Cmd>BufferLineMovePrev<CR>" },
-      { "<A->>", "<Cmd>BufferLineMoveNext<CR>" },
-      -- Goto buffer in position...
-      { "<A-1>", "<Cmd>BufferLineGoToBuffer  1<CR>" },
-      { "<A-2>", "<Cmd>BufferLineGoToBuffer  2<CR>" },
-      { "<A-3>", "<Cmd>BufferLineGoToBuffer  3<CR>" },
-      { "<A-4>", "<Cmd>BufferLineGoToBuffer  4<CR>" },
-      { "<A-5>", "<Cmd>BufferLineGoToBuffer  5<CR>" },
-      { "<A-6>", "<Cmd>BufferLineGoToBuffer  6<CR>" },
-      { "<A-7>", "<Cmd>BufferLineGoToBuffer  7<CR>" },
-      { "<A-8>", "<Cmd>BufferLineGoToBuffer  8<CR>" },
-      { "<A-9>", "<Cmd>BufferLineGoToBuffer  9<CR>" },
-      { "<A-0>", "<Cmd>BufferLineGoToBuffer -1<CR>" },
-      -- Close buffer pick
-      { "<A-c>", "<Cmd>BufferLinePickClose<CR>" },
-    },
     opts = {
       options = {
         style_preset = {
