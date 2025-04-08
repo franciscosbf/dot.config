e28540b897feb3d80b4460687c69395b2c5d2bf0
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2025-04-08 14:55:44 +0100

    nvim: remove line length marker

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- add line length marker
-vim.opt.colorcolumn = "99"
-
 -- add python dedicated environment with pynvim installed.
 vim.g.python3_host_prog = "~/.config/nvim-env/bin/python3"
 
