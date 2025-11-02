98d8543f71291e413b6f1fc8107724eb57251fb3
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2025-11-02 14:50:31 +0000

    nvim: set tab to 4 spaces

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,3 +10,12 @@ vim.g.loaded_ruby_provider = 0
 
 -- disable perl provider
 vim.g.loaded_perl_provider = 0
+
+-- A TAB character looks like 4 spaces
+vim.o.tabstop = 4
+-- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.expandtab = true
+-- Number of spaces inserted instead of a TAB character
+vim.o.softtabstop = 4
+-- Number of spaces inserted when indenting
+vim.o.shiftwidth = 4
