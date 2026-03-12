642a3bf751e53aeb7c70d019bfa829b68a16395b
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2026-03-12 13:05:09 +0000

    nvim: replace markdown plugin with custom configs

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     -- import/override with your plugins
     { import = "plugins" },
