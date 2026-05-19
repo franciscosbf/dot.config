b1936ad8f04a6b6615d3de2605487ae0c77f700c
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2026-05-19 14:38:29 +0100

    nvim: remove gopls workaround

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -16,8 +16,13 @@ return {
           filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hpp" },
         },
       },
-
       inlay_hints = { enabled = false },
+      setup = {
+        gopls = function(_, _)
+          -- remove workaround https://www.lazyvim.org/extras/lang/go#nvim-lspconfig
+          -- contains buggy code for something I don't even use and care.
+        end,
+      },
     },
   },
 }
